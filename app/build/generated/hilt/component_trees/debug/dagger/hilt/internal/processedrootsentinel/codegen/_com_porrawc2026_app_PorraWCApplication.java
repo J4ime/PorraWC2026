06eac15bb4c1201fd44eef86a3b290d766a3aa5f
J4ime <demariasanchez@gmail.com>
2026-05-29 16:42:52 +0200

    chore: add app/build/ to gitignore to reduce repo size

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_porrawc2026_app_PorraWCApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.porrawc2026.app.PorraWCApplication"
-)
-public final class _com_porrawc2026_app_PorraWCApplication {
-}

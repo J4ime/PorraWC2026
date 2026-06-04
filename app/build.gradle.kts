8a194ca5f7ec75724e4d97cf6b3821e9eb218c16
J4ime <demariasanchez@gmail.com>
2026-06-04 18:46:10 +0200

    release: v0.0.47 - Back to logo_porra as launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.porrawc2026.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 46
-        versionName = "0.0.46"
+        versionCode = 47
+        versionName = "0.0.47"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

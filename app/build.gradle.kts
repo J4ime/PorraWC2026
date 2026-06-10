6409d5b9becb029527982042556462e85f165788
J4ime <demariasanchez@gmail.com>
2026-06-10 16:51:34 +0200

    release: v0.0.86 - GridOn siempre en boton Excel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.porrawc2026.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 85
-        versionName = "0.0.85"
+        versionCode = 86
+        versionName = "0.0.86"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

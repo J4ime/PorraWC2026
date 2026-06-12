d6110a6b9654775ec50ee3d7b013ca105f44fd2a
J4ime <demariasanchez@gmail.com>
2026-06-12 20:02:10 +0200

    v1.4.3: tests + no try/catch + always refresh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.porrawc2026.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 135
-        versionName = "1.4.2"
+        versionCode = 136
+        versionName = "1.4.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

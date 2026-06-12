33188201495a5d9c1b72526a2fe7604ef968033f
J4ime <demariasanchez@gmail.com>
2026-06-13 00:32:45 +0200

    v1.5.4 bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.porrawc2026.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 147
-        versionName = "1.5.3"
+        versionCode = 148
+        versionName = "1.5.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

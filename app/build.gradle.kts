b15188fb5c4731dd904b6dafaa80158859652dcb
J4ime <demariasanchez@gmail.com>
2026-06-14 15:36:40 +0200

    v1.8.6: bump version for OTA update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.porrawc2026.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 169
-        versionName = "1.8.5"
+        versionCode = 170
+        versionName = "1.8.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "FOOTBALL_DATA_API_KEY", "\"${localProperties.getProperty("FOOTBALL_DATA_API_KEY", "")}\"")

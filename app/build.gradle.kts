4e96adeca719a407daf2af7e24af71c142eff669
J4ime <demariasanchez@gmail.com>
2026-07-06 14:13:49 +0200

    bump to 1.9.181

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.porrawc2026.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 345
-        versionName = "1.9.180"
+        versionCode = 346
+        versionName = "1.9.181"
         buildConfigField("String", "FOOTBALL_DATA_API_KEY", "\"${localProperties.getProperty("FOOTBALL_DATA_API_KEY", "")}\"")
     }
 

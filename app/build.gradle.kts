923f34991fbcd66d57cc27db0d61babfb1a7714b
J4ime <demariasanchez@gmail.com>
2026-07-09 20:19:26 +0200

    v1.9.204

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.porrawc2026.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 368
-        versionName = "1.9.203"
+        versionCode = 369
+        versionName = "1.9.204"
         buildConfigField("String", "FOOTBALL_DATA_API_KEY", "\"${localProperties.getProperty("FOOTBALL_DATA_API_KEY", "")}\"")
     }
 
